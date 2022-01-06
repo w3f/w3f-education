dc1cb3953645ca65975a0bb1fa38abe0560af515
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-01-06 17:46:26 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://github.com/DrW3RK/polkadot-mooc">
+            to="https://github.com/w3f/w3f-education">
             Contribute
           </Link>
         </div>
