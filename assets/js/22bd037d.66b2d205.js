e5bf53042d23cf0e27b833a3ba9700950b684b85
w3fdeploy <w3fdeply@users.noreply.github.com>
2022-10-12 22:15:28 +0000

    Deploy website - based on af6b50ee93c1cd2ba02dbd0f7f7938298d327fdf

---
+++ assets/js/22bd037d.66b2d205.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkw3f_education=self.webpackChunkw3f_education||[]).push([[5736],{106:function(o){o.exports=JSON.parse('{"allTagsPath":"/w3f-education/blog/tags","slug":"mooc","name":"mooc","count":1,"permalink":"/w3f-education/blog/tags/mooc"}')}}]);
--- assets/js/ff117942.4649589c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpolkadot_mooc=self.webpackChunkpolkadot_mooc||[]).push([[6922],{3408:function(o){o.exports=JSON.parse('{"permalink":"/w3f-education/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
