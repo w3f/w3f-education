76446ea052d4153f50f8d3df8962a652dd39adc5
Matteo Casonato <matteocasonato@protonmail.com>
2023-05-25 12:49:50 +0200

    Edit home
    - change Initiatives to Overview
    - fix css
    - moved wiki link inside Navbar

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,9 +10,29 @@ const FeatureList = [
     link: '/w3f-education/docs/introblock',
   },
   {
-    image: '/img/wiki.webp',
-    title: 'The Polkadot Wiki',
-    link: 'https://wiki.polkadot.network/',
+    image: '/img/blockchain_and_crypto.jpg',
+    title: 'Cryptography and Blockchain',
+    link: '/w3f-education/docs/Blockchain/Module2/cryptography',
+  },
+  {
+    image: '/img/hardforks.jpg',
+    title: 'Hardforks of Blockchain',
+    link: '/w3f-education/docs/Blockchain/Module3/forking',
+  },
+  {
+    image: '/img/distributed.jpg',
+    title: 'Introduction to Distributed Systems',
+    link: '/w3f-education/docs/Blockchain/Module4/distributed',
+  },
+  {
+    image: '/img/layers.jpg',
+    title: 'What are Blockchain Layers?',
+    link: '/w3f-education/docs/Blockchain/Module5/layers',
+  },
+  {
+    image: '/img/defi.jpg',
+    title: 'Cryptocurrencies and DeFi',
+    link: '/w3f-education/docs/Blockchain/Module6/crypto-defi',
   },
 ];
 
@@ -41,7 +61,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container container-home">
-        <h2 className="h2-home">Initiatives</h2>
+        <h2 className="h2-home">Overview</h2>
         <div className="row feature-row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
